da83701dfaa6f652b7426059dfaf20020bfb126b
alex2wong <huangyixiu188@hotmail.com>
2025-02-23 20:48:33 +0800

    fix: audioPlayer 文件不存在提示

--- src/components/MediaPlayer.jsx
+++ src/components/MediaPlayer.jsx
@@ -6,6 +6,7 @@ const MediaPlayer = ({ audioUrl }) => {
   const audioRef = useRef(new Audio(audioUrl));
   const [progress, setProgress] = useState(0);
   const [isPlaying, setIsPlaying] = useState(false);
+  const [isError, setIsError] = useState(false);
 
   // 更新进度条
   const updateProgress = () => {
@@ -43,41 +44,53 @@ const MediaPlayer = ({ audioUrl }) => {
   const handlePaused = () => {
     setIsPlaying(false)
   }
+  
+  const handleError = (e) => {
+    console.warn(`audio error: ${JSON.stringify(e)}`)
+    setIsError(true);
+  }
 
   useEffect(() =>{
     if (audioRef.current) {
       audioRef.current.addEventListener('timeupdate', updateProgress);
       audioRef.current.addEventListener('pause', handlePaused);
+      audioRef.current.addEventListener('error', handleError);
       audioRef.current.preload = 'auto';
       audioRef.current.load();
     }
     return () => {
-      audioRef.current.removeEventListener('timeupdate', updateProgress);
-      audioRef.current.removeEventListener('pause', handlePaused);
+      if (audioRef.current) {
+        audioRef.current.removeEventListener('timeupdate', updateProgress);
+        audioRef.current.removeEventListener('pause', handlePaused);
+        audioRef.current.pause();
+      }
     }
   }, [])
 
-  return (
+  const renderAudioPlayer = () => (
     <div className="audio-player" style={{
       display: 'flex',
       width: '100%',
-      maxWidth: '200px',
+      maxWidth: '240px',
       justifyContent: 'space-between',
       alignItems: 'center',
     }}>
-      <div onClick={handleBtnClick} style={{ cursor: 'pointer' }}>{ isPlaying ? '⏸️' : '▶️' }</div>
-      <div className="progress-bar" style={{
-        width: '100%',
-        height: '8px',
-        marginLeft: 8,
-        backgroundColor: '#f0f0f0',
-        borderRadius: 8,
-        overflow: 'hidden'
-      }} onClick={handleProgressClick}>
-        <div className="progress" style={{ width: `${progress}%`, height:'8px', backgroundColor: 'green' }}></div>
-      </div>
+        <div onClick={handleBtnClick} style={{ cursor: 'pointer', width: 24 }}>{ isPlaying ? '⏸️' : '▶️' }</div>
+        <div className="progress-bar" style={{
+          width: '100%',
+          height: '8px',
+          marginLeft: 12,
+          backgroundColor: '#f0f0f0',
+          borderRadius: 8,
+          overflow: 'hidden'
+        }} onClick={handleProgressClick}>
+          <div className="progress" style={{ width: `${progress}%`, height:'8px', backgroundColor: 'green' }}></div>
+        </div>
     </div>
   );
+
+  return isError? <div style={{ backgroundColor:'orange', padding: 2, borderRadius: 4 }}>音频加载失败</div> : renderAudioPlayer();
+
 };
 
 export default MediaPlayer;
