f617011b6f9de9248f04f8de03e101d34fb3d3ca
lolieatapple <lolieatapple@gmail.com>
2025-03-04 18:07:02 +0800

    style: update UI style by sonnet 3.7

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App.jsx'
 import SongDetailPage from './pages/SongDetail/index.jsx';
 // import './index.css'
+import './styles/theme.css'
 
 const router = createBrowserRouter([
   {
@@ -20,4 +21,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>
-) 
+)
