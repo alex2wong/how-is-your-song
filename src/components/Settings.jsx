774030ac8714ca9a6c2fa92734bc85cef51809b7
lolieatapple <lolieatapple@gmail.com>
2025-03-26 16:23:32 +0800

    feat: add gemini-2.5-pro-exp-03-25 support

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -129,6 +129,7 @@ export default function Settings({ children }) {
             >
               <option value="gemini-2.0-pro-exp-02-05">gemini-2.0-pro-exp-02-05</option>
               <option value="gemini-2.0-flash">gemini-2.0-flash</option>
+              <option value="gemini-2.5-pro-exp-03-25">gemini-2.5-pro-exp-03-25</option>
             </select>
           </div>
           
